bf6869e4de571f3f6503a420caea804bc4cc0ffa
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-19 15:58:27 -0300

    feat: create a custom Theme

---
+++ configs/theme.ts
@@ -0,0 +1,11 @@
+// import '@fontsource/open-sans/700.css'
+import { extendTheme } from "@chakra-ui/react"
+
+
+
+export default extendTheme({
+    fonts: {
+        body: 'Open Sans, sans-serif',
+        heading: 'Open Sans, sans-serif'
+      }
+})
