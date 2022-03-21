426ee4f7c4bcb19becc79548716b3f7923429178
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-21 19:30:35 -0300

    feat: add public env variable in axios config

--- configs/axios.ts
+++ configs/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
-
 const http = axios.create({
-    baseURL: process.env.BASE_URL ?? 'http://localhost:8080/api'
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL ? process.env.NEXT_PUBLIC_BASE_URL: 'http://localhost:8080/api'
 })
 
 export { http }
