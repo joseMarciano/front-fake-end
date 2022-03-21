01c45851608655765a20eef405d2f29201db4e02
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-20 21:38:31 -0300

    feat: create a generic input form

---
+++ components/formInput/index.tsx
@@ -0,0 +1,35 @@
+import { FormErrorMessage, FormLabel, FormLabelProps, Input, InputProps } from "@chakra-ui/react"
+import { FieldValues, FormState, UseFormRegister } from "react-hook-form"
+
+
+type FormInputProps = {
+    formLabelProps?: Omit<FormLabelProps, 'htmlFor'>
+    inputProps?: Omit<InputProps, 'id'>
+    useFormRegister?: UseFormRegister<FieldValues>
+    formState?: FormState<FieldValues>
+    name: string
+    label?: string
+}
+
+export default function FormInput({
+    formLabelProps,
+    inputProps,
+    name,
+    label,
+    useFormRegister,
+    formState
+
+}: FormInputProps) {
+    const useFormRegiterFn = useFormRegister ? useFormRegister : () => { }
+
+
+    return (
+        <>
+            {label &&
+                <FormLabel m="0" htmlFor={name} {...formLabelProps}>{label}</FormLabel>
+            }
+            <Input id={name} {...inputProps} {...useFormRegiterFn(name)} />
+            {formState && 
+            <FormErrorMessage mt="2px">{formState.errors[name]?.message}</FormErrorMessage>}
+        </>)
+} 
