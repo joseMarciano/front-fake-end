656f7ffabbce070356dd9ecec07cab41cd4b2a2c
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-21 18:47:06 -0300

    feat: finish signUp page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -4,8 +4,33 @@ import { useForm } from "react-hook-form"
 import * as yup from "yup"
 import { yupResolver } from '@hookform/resolvers/yup'
 import { AiOutlineLock } from 'react-icons/ai'
+import { ReactNode, useState } from "react"
+import { useHttpContext } from "../../contexts/HttpContext"
+import { useRouter } from "next/router"
 
 
+const FIELDS = [
+    {
+        label: 'Email',
+        name: 'email',
+        type: 'email'
+    },
+    {
+        label: 'Name',
+        name: 'name',
+        type: 'text'
+    },
+    {
+        label: 'Password',
+        name: 'password',
+        type: 'password'
+    },
+    {
+        label: 'Password confirmation',
+        name: 'passwordConfirmation',
+        type: 'password'
+    }
+]
 
 const schema = yup.object().shape({
     name: yup.string().required("Name is required"),
@@ -14,10 +39,16 @@ const schema = yup.object().shape({
     passwordConfirmation: yup.string().required("Password confirmation is required")
 })
 
+
 export default function SignUp() {
+    const { http } = useHttpContext()
+    const router = useRouter()
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(schema)
     })
+
+    const [isLoading, setIsLoading] = useState(false)
     const toast = useToast()
 
     const onSubmit = (data: any) => {
@@ -28,11 +59,37 @@ export default function SignUp() {
                 status: 'error'
             })
         }
+
+        setIsLoading(true)
+        http.post('/signup', data)
+            .then(() => toast({ description: 'Check your email to activate your account', status: 'info' }))
+            .then(() => router.push('/login'))
+            .catch((error) => console.warn(error))
+            .finally(() => setIsLoading(false))
     }
 
     const validateFields = (data: any): boolean => {
         return data.password === data.passwordConfirmation
     }
+
+
+    function renderField({ label, name, type }: { label: string, name: string, type: string }, index: number): ReactNode {
+        return (
+            <Box width="80%" maxW="500px" key={index}>
+                <FormInput
+                    label={label}
+                    name={name}
+                    formState={formState}
+                    useFormRegister={register}
+                    inputProps={{
+                        type,
+                        isDisabled: isLoading
+                    }}
+                />
+            </Box>
+        )
+    }
+
     return (
         <VStack height="100vh" justifyContent="center">
             <Image
@@ -48,49 +105,9 @@ export default function SignUp() {
                 isRequired
             >
                 <VStack spacing={5} justifyContent="center">
-                    <Box width="80%" maxW="500px">
-                        <FormInput
-                            label="Email"
-                            name="email"
-                            formState={formState}
-                            useFormRegister={register}
-                            inputProps={{
-                                type: 'email'
-                            }}
-                        />
-                    </Box>
-                    <Box width="80%" maxW="500px">
-                        <FormInput
-                            label="Name"
-                            name="name"
-                            formState={formState}
-                            useFormRegister={register}
-                        />
-                    </Box>
-                    <Box width="80%" maxW="500px">
-                        <FormInput
-                            label="Password"
-                            name="password"
-                            formState={formState}
-                            useFormRegister={register}
-                            inputProps={{
-                                type: 'password'
-                            }}
-                        />
-                    </Box>
-                    <Box width="80%" maxW="500px">
-                        <FormInput
-                            label="Password confirmation"
-                            name="passwordConfirmation"
-                            formState={formState}
-                            useFormRegister={register}
-                            inputProps={{
-                                type: 'password'
-                            }}
-                        />
-                    </Box>
+                    {FIELDS.map(renderField)}
                     <Box >
-                        <Button leftIcon={<AiOutlineLock />} bg="gray.600" color="white" type="submit">Register</Button>
+                        <Button isLoading={isLoading} leftIcon={<AiOutlineLock />} colorScheme="blue" color="white" type="submit">Register</Button>
                     </Box>
                 </VStack>
             </FormControl>
