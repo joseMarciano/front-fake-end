e5e6a64402d3e97c0ce94c6b85cd16a48288b8e8
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-22 23:53:36 -0300

    feat: create a storage hook

---
+++ hooks/useStorage.ts
@@ -0,0 +1,41 @@
+
+enum StorageKeys {
+    REFRESH_TOKEN = 'REFRESH_TOKEN',
+    ACCESS_TOKEN = 'ACCESS_TOKEN'
+}
+
+type UseStorageReturnProps = {
+    setAccessToken: (accessToken: string) => void,
+    setRefreshToken: (refreshToken: string) => void,
+    getAccessToken: () => string | null,
+    getRefreshToken: () => string | null
+}
+
+
+export function useStorage(): UseStorageReturnProps {
+
+    const setAccessToken = (accessToken: string): void => {
+        localStorage.setItem(StorageKeys.ACCESS_TOKEN, accessToken)
+    }
+
+
+    const setRefreshToken = (refreshToken: string): void => {
+        localStorage.setItem(StorageKeys.REFRESH_TOKEN, refreshToken)
+
+    }
+
+    const getAccessToken = (): string | null => {
+        return localStorage.getItem(StorageKeys.ACCESS_TOKEN)
+    }
+
+    const getRefreshToken = (): string | null => {
+        return localStorage.getItem(StorageKeys.REFRESH_TOKEN)
+    }
+
+    return {
+        setAccessToken,
+        setRefreshToken,
+        getAccessToken,
+        getRefreshToken
+    }
+}
