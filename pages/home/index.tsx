6d12edefdaf97e0a386c06ec2d2ac5107cb0b858
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-26 18:08:17 -0300

    feat: add useBreakPointValue to make responsive list

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,9 +1,30 @@
+import { Button, Flex, useBreakpointValue } from "@chakra-ui/react"
 import { ReactNode } from "react"
+import { FiRefreshCcw } from 'react-icons/fi'
 import CommonLayoutWrapper from "../../components/commonLayoutWrapper"
+import ProjectList from '../../components/home/project/list/index'
+
+
+const bsVariant = { padding: '0' }
+const smVariant = { padding: '0' }
+const mdVariant = { padding: '0' }
+const lgVariant = { padding: '5rem' }
+
 
 export default function Home() {
+    const variants = useBreakpointValue({
+        sm: smVariant,
+        base: bsVariant,
+        md: mdVariant,
+        lg: lgVariant
+    })
 
-    return <h1>Aqui é o componente home</h1>
+    return (
+        <Flex m="0.5rem" p={variants?.padding} flexDirection="column" gap="0.25rem">
+            <Button alignSelf="flex-end" colorScheme="teal" leftIcon={<FiRefreshCcw />} m="0.5rem">Refresh</Button>
+            <ProjectList />
+        </Flex>
+    )
 }
 
 Home.getLayout = function getLayout(page: ReactNode) {
