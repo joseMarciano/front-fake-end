3b076bab7e5a47bfd1c44cdb75415736b77bc147
joseMarciano <marcianojosepaulo@gmail.com>
2022-03-27 18:23:29 -0300

    feat: open modal by add project button

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,8 +1,10 @@
 import { Button, Flex, useBreakpointValue } from "@chakra-ui/react"
-import { ReactNode } from "react"
-import { FiRefreshCcw } from 'react-icons/fi'
+import { ReactNode, useEffect } from "react"
+import { FiRefreshCcw, FiPlus } from 'react-icons/fi'
 import CommonLayoutWrapper from "../../components/commonLayoutWrapper"
 import ProjectList from '../../components/home/project/list/index'
+import { ProjectModal } from "../../components/home/project/modal"
+import { useModalContext } from "../../contexts/ContextModal"
 
 
 const bsVariant = { padding: '0' }
@@ -19,11 +21,26 @@ export default function Home() {
         lg: lgVariant
     })
 
+    const { actions, setTemplate, setParams, params } = useModalContext()
+
+    useEffect(() => {
+        setParams({
+            ...params,
+            title: 'Adding a project'
+        })
+        setTemplate(<ProjectModal />)
+    }, [])
+
+
     return (
         <Flex m="0.5rem" p={variants?.padding} flexDirection="column" gap="0.25rem">
-            <Button alignSelf="flex-end" colorScheme="teal" leftIcon={<FiRefreshCcw />} m="0.5rem">Refresh</Button>
+            <Flex alignItems="center" justifyContent="space-between">
+                <Button alignSelf="flex-end" onClick={() => actions.open()} colorScheme="teal" leftIcon={<FiPlus />} m="0.5rem">Project</Button>
+                <Button alignSelf="flex-end" colorScheme="teal" leftIcon={<FiRefreshCcw />} m="0.5rem">Refresh</Button>
+            </Flex>
             <ProjectList />
         </Flex>
+
     )
 }
 
